14feaeccfe831a01e9f32fee42961d14dc6f63a8
Sam Rooney <the.sam.rooney@gmail.com>
2025-04-23 02:45:15 -0400

    add hardware passthrough and host network to docker container

--- run_ros_image.sh
+++ run_ros_image.sh
@@ -1,4 +1,16 @@
+#!/bin/bash
+set -e
 
+# Enable X11 access for Docker
 xhost +local:docker
-docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix $1
 
+# Run Docker container with X11, GPU, and network support
+docker run -it \
+  --env DISPLAY=$DISPLAY \
+  --volume /tmp/.X11-unix:/tmp/.X11-unix \
+  --device /dev/dri:/dev/dri \
+  --network host \
+  $1
+
+# Revoke X11 access after container stops
+xhost -local:docker
