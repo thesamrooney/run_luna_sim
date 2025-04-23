fd8f1a03fb0f96f5c58314ca2b50be02f245e5f3
Sam Rooney <the.sam.rooney@gmail.com>
2025-04-23 02:44:50 -0400

    confirm presence of OpenGL support in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,14 @@ set -e
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
 source "/ros2_ws/install/setup.bash" --
 
+# Enable verbose logging for Gazebo (optional, for debugging)
+export GZ_SIM_VERBOSE=1
+
+# Check OpenGL support
+if glxinfo | grep -q "OpenGL version"; then
+  echo "OpenGL support detected."
+else
+  echo "Warning: OpenGL support not detected. Gazebo GUI may fail."
+fi
+
 exec "$@"
